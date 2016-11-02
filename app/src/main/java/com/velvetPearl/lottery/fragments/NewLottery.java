0fdba712c80f306ec131f611a244819f5949b11d
Andreas S. Jensen <stensig04@gmail.com>
2016-11-02 12:07:48 +0100

    New lottery view WIP

---
+++ app/src/main/java/com/velvetPearl/lottery/fragments/NewLottery.java
@@ -0,0 +1,21 @@
+package com.velvetPearl.lottery.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Stensig on 02-Nov-16.
+ */
+
+public class NewLottery extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}

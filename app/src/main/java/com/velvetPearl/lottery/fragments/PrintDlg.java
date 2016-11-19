f055351b16dd5d1181b28ff8e2f5164943242120
Andreas S. Jensen <stensig04@gmail.com>
2016-11-19 18:06:24 +0100

    Print to xml file in progress.

--- app/src/main/java/com/velvetPearl/lottery/fragments/PrintDlg.java
+++ app/src/main/java/com/velvetPearl/lottery/fragments/PrintDlg.java
@@ -1,8 +1,15 @@
 package com.velvetPearl.lottery.fragments;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,24 +17,59 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.velvetPearl.lottery.ApplicationDomain;
 import com.velvetPearl.lottery.R;
 import com.velvetPearl.lottery.dataAccess.models.Lottery;
+import com.velvetPearl.lottery.dataAccess.models.LotteryNumber;
+import com.velvetPearl.lottery.dataAccess.models.Prize;
+import com.velvetPearl.lottery.dataAccess.models.Ticket;
+
+import org.xmlpull.v1.XmlSerializer;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
 
 /**
  * Created by Stensig on 19-Nov-16.
  */
 
-public class PrintDlg extends DialogFragment {
+public class PrintDlg extends DialogFragment implements View.OnClickListener {
 
+    private static final String LOG_TAG = "DialogFragment";
+
+    // XML tags and attributes names
+    private static final String NAMESPACE = null;
+    private static final String ID_ATTR = "Id";
+    private static final String LOTTERY_ROOT_TAG = "VelvetPearLottery";
+    private static final String NAME_TAG = "Name";
+    private static final String CREATED_TAG = "Created";
+    private static final String RANGE_MIN_TAG = "MinLotteryNumber";
+    private static final String RANGE_MAX_TAG = "MaxLotteryNumber";
+    private static final String TICKETS_ROOT_TAG = "Tickets";
+    private static final String TICKET_TAG = "Ticket";
+    private static final String TICKET_NUMBERS_ROOT_TAG = "LotteryNumbers";
+    private static final String TICKET_NUMBER_TAG = "LotteryNumber";
+    private static final String TICKET_NUMBER_PRICE_ATTR = "Price";
+    private static final String TICKET_OWNER_TAG = "Owner";
+    private static final String PRIZES_ROOT_TAG = "Prizes";
+    private static final String PRIZE_TAG = "Prize";
+    private static final String PRIZE_NUMBER_ATTR = "LotteryNumber";
+
+
+    private static final String FILE_EXTENSION = ".vplf";
+    private static final int FILE_EXTENSION_LENGTH = FILE_EXTENSION.length();
     private File outputFile;
     private Lottery lottery;
+    private AsyncTask printingTask;
+    private ProgressDialog printingProgressDlg;
 
     // UI elements
     private Button cancelBtn;
+    private Button confirmBtn;
     private Switch storageDeviceSwitch;
     private EditText saveAsInput;
 
@@ -44,12 +86,9 @@ public class PrintDlg extends DialogFragment {
 
     private void initUI(View root) {
         cancelBtn = (Button) root.findViewById(R.id.print_cancel);
-        cancelBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getFragmentManager().popBackStack();
-            }
-        });
+        cancelBtn.setOnClickListener(this);
+        confirmBtn = (Button) root.findViewById(R.id.print_confirm);
+        confirmBtn.setOnClickListener(this);
 
         storageDeviceSwitch = (Switch) root.findViewById(R.id.print_storage_device);
         storageDeviceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -66,4 +105,174 @@ public class PrintDlg extends DialogFragment {
         saveAsInput = (EditText) root.findViewById(R.id.print_save_as);
         saveAsInput.setText(lottery.getName());
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view == cancelBtn) {
+            getFragmentManager().popBackStack();
+        } else if (view == confirmBtn) {
+            // Get filename
+            String fileName = saveAsInput.getText().toString();
+            if (fileName == null || fileName.isEmpty()) {
+                Toast.makeText(getContext(), R.string.missing_file_name, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Add file extension (.vplf) if missing
+            if (fileName.length() < FILE_EXTENSION_LENGTH || !fileName.substring(fileName.length() - FILE_EXTENSION_LENGTH, fileName.length()).equals(FILE_EXTENSION)) {
+                fileName += FILE_EXTENSION;
+            }
+
+            // Open file handle either on external or internal storage
+            if (storageDeviceSwitch.isChecked()) {
+                if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+                    Toast.makeText(getContext(), R.string.no_external_drive, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                outputFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), fileName);
+            } else {
+                outputFile = new File(getContext().getFilesDir(), fileName);
+            }
+
+            // Don't overwrite existing file without warning
+            if (outputFile.exists()) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext(), R.style.AppTheme_Dialog_Alert);
+                builder.setMessage(R.string.file_exists)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                printLotteryToFile();
+                            }
+                        })
+                        .setNegativeButton(R.string.no, null);
+                builder.create().show();
+            } else {
+                printLotteryToFile();
+            }
+        }
+    }
+
+    private void printLotteryToFile() {
+        printingProgressDlg = ProgressDialog.show(getContext(), null, getString(R.string.print_to_file_progress_message), false, true, new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                printingTask.cancel(true);
+                printingProgressDlg.dismiss();
+                getFragmentManager().popBackStack();
+            }
+        });
+        printingProgressDlg.setProgress(0);
+
+        printingTask = new AsyncTask() {
+            @Override
+            protected void onPostExecute(Object o) {
+                printingProgressDlg.dismiss();
+                Toast.makeText(getContext(), R.string.print_to_file_finished, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            protected void onProgressUpdate(Object[] values) {
+                if (printingProgressDlg != null && printingProgressDlg.isShowing()) {
+                    Log.d(LOG_TAG, String.format("Updating progress by %d", (int)values[0]));
+                    printingProgressDlg.incrementProgressBy((int)values[0]);
+                }
+            }
+
+            @Override
+            protected void onCancelled(Object o) {
+                Toast.makeText(getContext(), R.string.print_to_file_cancelled, Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                if (lottery == null || isCancelled()) {
+                    return null;
+                }
+
+                Map<Object, Ticket> tickets = lottery.getTickets();
+                Map<Object, Prize> prizes = lottery.getPrizes();
+
+                // Calculate the weight of each step in the process, with progress dialog being full at 1000 elements
+                int progressWeight =  1000 / (tickets.size() + prizes.size());
+
+                XmlSerializer xmlWriter = Xml.newSerializer();
+                StringWriter stringWriter = new StringWriter();
+
+
+
+                try {
+                    xmlWriter.setOutput(stringWriter);
+                    xmlWriter.startDocument("UTF-8", true);
+                    xmlWriter.startTag(NAMESPACE, LOTTERY_ROOT_TAG);
+
+                    xmlWriter.attribute(NAMESPACE, ID_ATTR, (String)lottery.getId());
+
+                    xmlWriter.startTag(NAMESPACE, NAME_TAG);
+                    xmlWriter.text(lottery.getName());
+                    xmlWriter.endTag(NAMESPACE, NAME_TAG);
+
+                    xmlWriter.startTag(NAMESPACE, CREATED_TAG);
+                    xmlWriter.text(Long.toString(lottery.getCreated()));
+                    xmlWriter.endTag(NAMESPACE, CREATED_TAG);
+
+                    xmlWriter.startTag(NAMESPACE, RANGE_MIN_TAG);
+                    xmlWriter.text(Integer.toString(lottery.getLotteryNumLowerBound()));
+                    xmlWriter.endTag(NAMESPACE, RANGE_MIN_TAG);
+
+                    xmlWriter.startTag(NAMESPACE, RANGE_MAX_TAG);
+                    xmlWriter.text(Integer.toString(lottery.getLotteryNumUpperBound()));
+                    xmlWriter.endTag(NAMESPACE, RANGE_MAX_TAG);
+
+                    xmlWriter.startTag(NAMESPACE, TICKETS_ROOT_TAG);
+                    for (Object ticketId : tickets.keySet()) {
+                        if (isCancelled()) {
+                            return null;
+                        }
+
+                        Ticket ticket = tickets.get(ticketId);
+                        if (ticket != null) {
+                            xmlWriter.startTag(NAMESPACE, TICKET_TAG);
+                            xmlWriter.attribute(NAMESPACE, ID_ATTR, (String)ticket.getId());
+
+                            xmlWriter.startTag(NAMESPACE, TICKET_NUMBERS_ROOT_TAG);
+                            for (LotteryNumber number : ticket.getLotteryNumbers()) {
+                                xmlWriter.startTag(NAMESPACE, TICKET_NUMBER_TAG);
+                                xmlWriter.attribute(NAMESPACE, ID_ATTR, (String)number.getId());
+                                xmlWriter.attribute(NAMESPACE, TICKET_NUMBER_PRICE_ATTR, Double.toString(number.getPrice()));
+                                xmlWriter.text(Integer.toString(number.getLotteryNumber()));
+                                xmlWriter.endTag(NAMESPACE, TICKET_NUMBER_TAG);
+                            }
+                            xmlWriter.endTag(NAMESPACE, TICKET_NUMBERS_ROOT_TAG);
+
+                            xmlWriter.startTag(NAMESPACE, TICKET_OWNER_TAG);
+                            xmlWriter.text(ticket.getOwner());
+                            xmlWriter.endTag(NAMESPACE, TICKET_OWNER_TAG);
+                            xmlWriter.endTag(NAMESPACE, TICKET_TAG);
+                        }
+                        publishProgress(progressWeight);
+                    }
+                    xmlWriter.endTag(NAMESPACE, TICKETS_ROOT_TAG);
+
+
+                    xmlWriter.endTag(NAMESPACE, LOTTERY_ROOT_TAG);
+
+
+                    // TODO xml datastream save to file
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), R.string.an_error_happened, Toast.LENGTH_SHORT);
+                    if (printingProgressDlg != null && printingProgressDlg.isShowing()) {
+                        printingProgressDlg.dismiss();
+                    }
+                    return null;
+                }
+
+                return null;
+            }
+        };
+
+        printingTask.execute();
+    }
+
 }
